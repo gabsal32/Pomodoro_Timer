a9feaf36c1fb93b74a1e52c62b79865da04b361a
Gabriel Salgado - ThinkPad <gabsal23@hotmail.com>
2026-03-29 10:23:58 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+# Put any imports and global variable here
+
+# 1. Set up a the basic parts for the Tkinter window?
+
+# Main logic? Timedate? Time? or TimeDelta?
+
+# If the file gets too long, maybe I'll turn the timer into its own class...
