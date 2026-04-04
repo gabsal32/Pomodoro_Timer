7b7fa135d5bc2a4b9ac6cc5d83d753f9474a1d77
Gabriel Salgado - ThinkPad <gabsal23@hotmail.com>
2026-04-04 17:19:04 -0500

    Reorganized App, Added icon
    I'm basing the layout on a YouTube video by Atlus, who used ttk widgets in his video. I also added a tomato icon for the app icon.

--- main.py
+++ main.py
@@ -1,20 +1,48 @@
 # Put any imports and global variable here
 import tkinter as tk
+from tkinter import ttk
 
-# 1. Set up a the basic parts for the Tkinter window?
+# Window
 root = tk.Tk()
 root.title("Pomodoro Timer")
+root.geometry('600x600')
+root.minsize(600,600)
 
+# App Icon
+icon = tk.PhotoImage(file="pom_icon.png")
+root.iconphoto(True, icon)
+
+# Main Layout Widgets
+menu_frame = ttk.Frame(root)
+main_frame = ttk.Frame(root)
+
+# Main place layout
+menu_frame.place(x = 0, y = 0,relwidth= 0.3, relheight = 1)
+main_frame.place(relx = 0.3, y = 0, relwidth = 0.7, relheight = 1)
+ttk.Label(menu_frame, background='red').pack(expand=True, fill='both')
+ttk.Label(main_frame, background='yellow').pack(expand=True, fill='both')
+
+# menu widgets
+start_button = ttk.Button(menu_frame, text = 'Start')
+pause_button = ttk.Button(menu_frame, text = 'Pause')
+reset_button = ttk.Button(menu_frame, text = 'Reset')
+
+# Things...
 welcome_msg = tk.Label(root, text="Welcome to my Pomodoro App!")
-welcome_msg.pack();
+welcome_msg.pack()
 
-stop_btn = tk.Button(root, text="Stop")
-stop_btn.pack()
+# work_label = tk.Label(root, text="Work Interval: ")
+# rest_title = tk.Label(root, text="Rest Interval: ")
+# work_interval = tk.Spinbox(root, from_=0, to=999)
+# rest_interval = tk.Spinbox(root, from_=0, to=999)
 
-work_interval = tk.Spinbox(root, from_=0, to=999)
-rest_interval = tk.Spinbox(root, from_=0, to=999)
-work_interval.pack()
-rest_interval.pack()
+# work_label.grid(row=0, column=0)
+# work_interval.grid(row=0,column=1)
+# rest_title.grid(row=1, column=0)
+# rest_interval.grid(row=1,column=1)
+
+# #stop_btn = tk.Button(root, text="Stop")
+# #stop_btn.pack()
 
 root.mainloop();
 # Main logic? Timedate? Time? or TimeDelta?
