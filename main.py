b2af4581a1b41385fc641be6d46cd1089d584990
Gabriel Salgado - ThinkPad <gabsal23@hotmail.com>
2026-05-07 17:06:47 -0500

    Moved methods inside PomodoroApp class; worked on start method.
    Moved the methods inside the PomodoroApp Class. I also began working on starting the timer.

--- main.py
+++ main.py
@@ -68,32 +68,17 @@ import time
 #root.mainloop();
 
 
-# Main logic? Timedate? Time? or TimeDelta?
-def start_timer():
-    # time remaining = time_now + (work_interval OR rest_interval) depending
-    # on which state we are in
-    #
-    print("the timer was started!")
-    return -1
-
-def pause_timer():
-    # add stuff here
-    print("The timer was paused!")
-    return -1
-
-def reset_timer():
-    # add stuff here
-    print("The timer was reset!")
-    return -1
-
-def countdown():
-    # add stuff here
-    print("Counting down...")
-    return -1
-
 # GUI 
-class PomodoroApp:
+class PomodoroApp:   
     def __init__(self, root):
+        """
+        Sets up the GUI window. It also initializes some of the status variables
+        
+        Args:
+            self: A PomodoroApp object. Passed automatically
+            root (Tkinter): A tkinter object
+        """
+        
         # status variables
         self.is_running = False
         self.is_paused = True # if it were false, it would conflict with is_running initially
@@ -118,15 +103,23 @@ class PomodoroApp:
         # ttk.Label(menu_frame, background='red').pack(expand=True, fill='both')
         # ttk.Label(main_frame, background='yellow').pack(expand=True, fill='both')
 
+        vcmd= root.register(self.validate_num)
+        
         # menu widgets
         work_title = ttk.Label(menu_frame, text="Work Interval: ", font=("Verdana", 12))
         rest_title = ttk.Label(menu_frame, text="Rest Interval: ", font=("Verdana", 12))
-        self.work_interval = ttk.Spinbox(menu_frame, from_=0, to=999, font=("Verdana", 12))
-        self.rest_interval = ttk.Spinbox(menu_frame, from_=0, to=999, font=("Verdana", 12))
+        # These spinboxes can only take in integer values
+        self.work_interval = ttk.Spinbox(menu_frame, from_=0, to=999, font=("Verdana", 12),
+                                         validate="key",
+                                         validatecommand=(vcmd, '%P'))
+        
+        self.rest_interval = ttk.Spinbox(menu_frame, from_=0, to=999, font=("Verdana", 12),
+                                         validate="key", 
+                                         validatecommand=(vcmd, '%P'))
 
-        self.start_button = ttk.Button(menu_frame, text = 'Start', command=start_timer)
-        self.pause_button = ttk.Button(menu_frame, text = 'Pause', command=pause_timer)
-        self.reset_button = ttk.Button(menu_frame, text = 'Reset', command=reset_timer)
+        self.start_button = ttk.Button(menu_frame, text = 'Start', command=self.start_timer)
+        self.pause_button = ttk.Button(menu_frame, text = 'Pause', command=self.pause_timer)
+        self.reset_button = ttk.Button(menu_frame, text = 'Reset', command=self.reset_timer)
 
         # Menu Layout
         menu_frame.columnconfigure((0,1), weight = 1, uniform = 'a')
@@ -156,6 +149,42 @@ class PomodoroApp:
         # Main layout 
         self.state_label.pack(side = 'top', expand = False, fill = 'none', pady= 40)
         self.timer_label.pack(side = 'top', expand = True, fill = 'none')
+        
+    def validate_num(self, char):
+        """
+        Determines if the value in a spinbox is a digit or not
+        """
+        return char.isdigit() or char == ""
+        
+    # Main logic? Timedate? Time? or TimeDelta?
+    def start_timer(self):
+        # time remaining = time_now + (work_interval OR rest_interval) depending
+        # on which state we are in
+        # try 
+        self.time_remaining = int(self.work_interval.get())
+        # except
+            # set the time to 0
+        
+        print("work interval: " + str(self.time_remaining) + " minutes")
+        
+        print("the timer was started!")
+        return -1
+
+    def pause_timer(self):
+        # add stuff here
+        print("The timer was paused!")
+        return -1
+
+    def reset_timer(self):
+        # add stuff here
+        print("The timer was reset!")
+        return -1
+
+    def countdown(self):
+        # add stuff here
+        print("Counting down...")
+        return -1
+
 
 # Set up the GUI
 def main():
